2bdb81947efe5a4f2a13b871657d1b84fbc773f4
Philipp <philipp1501@gmx.de>
2020-05-02 19:10:33 +0200

    Spracheingabe

--- app/src/main/java/com/example/newtut/MainActivity.java
+++ app/src/main/java/com/example/newtut/MainActivity.java
@@ -18,9 +18,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.PowerManager;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
@@ -35,13 +38,11 @@ import android.view.MenuItem;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    MediaRecorder mediaRecorder;
-    boolean isRecording = false;
-    boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,110 +51,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 43);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 42);
-        }
     }
 
-    public void startRecord(View view) {
-
-        mediaRecorder = new MediaRecorder();
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mediaRecorder.setOutputFile(this.getExternalFilesDir(null).getAbsolutePath() + "/aufnahme.3gp");
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
+    public void sprache(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                .putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                .putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                .putExtra(RecognizerIntent.EXTRA_PROMPT, "Jetzt reden!");
         try {
-            mediaRecorder.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mediaRecorder.start();
-        isRecording = true;
-    }
-
-    public void stopRecord(View view){
-        if(isRecording) {
-            mediaRecorder.stop();
-            mediaRecorder.release();
-            isRecording = false;
+            startActivityForResult(intent, 100);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "Speech not supprted", Toast.LENGTH_LONG).show();
         }
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void playMedia(View view) {
-        if(!isPlaying) {
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            Uri uri = Uri.parse(this.getExternalFilesDir(null).getAbsolutePath() + "/aufnahme.3gp");
-            WifiManager.WifiLock wifiLock = ((WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE)).createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "42");
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH) //CONTENT_TYPE_MUSIC, CONTENT_TYPE_VIDEO; CONTENT_TYPE_UNKNOWN,...
-                    .build();
-            mediaPlayer.setAudioAttributes(audioAttributes);
-            try {
-                mediaPlayer.setDataSource(getApplicationContext(), uri);
-                mediaPlayer.prepare();
-                mediaPlayer.setScreenOnWhilePlaying(true);
-                //mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-                wifiLock.acquire();
-                mediaPlayer.start();
-                isPlaying = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    mp.release();
-                    isPlaying = false;
-                }
-            });
-            wifiLock.release();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode == RESULT_OK && data != null){
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            ((TextView)findViewById(R.id.textView)).setText(result.get(0));
         }
-    }
-
-    public void sendEmail(View view){
-        String[] to = {"philipp1501@gmx.de", "jani-philipp@gmx.de"};
-        String[] cc = {"jani.knapp@gmx.de"};
-
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        //intent.setDataAndType(Uri.parse("mailto"), "text/plain");
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_EMAIL, to);
-        intent.putExtra(Intent.EXTRA_CC, cc);
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Ich bin der Betreff");
-        intent.putExtra(Intent.EXTRA_TEXT, "Email-text hier");
-
-        startActivity(intent);
-        finish();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void myNotification(View view) {
-        String channelId = "My_channel_id";
-        Notification.Builder builder = new Notification.Builder(this, channelId)
-                .setContentTitle("Notification")
-                .setContentText("Ich bin eine Notification")
-                .setSmallIcon(R.drawable.ic_message)
-                .setCategory(Notification.CATEGORY_PROGRESS)
-                .setAutoCancel(true);
-
-        Intent intent = new Intent(this, NotificationActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.putExtra("message", "Ich bin eine neue Activity");
-        PendingIntent pendingIntent =   PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        NotificationChannel channel = new NotificationChannel(channelId, "name of channel", NotificationManager.IMPORTANCE_DEFAULT);
-        notificationManager.createNotificationChannel(channel);
-        builder.setChannelId(channelId);
-
-        notificationManager.notify(0, builder.build());
     }
 
     @Override
