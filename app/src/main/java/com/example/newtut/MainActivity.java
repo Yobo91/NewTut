c18fec6de458cca94c9789006d944ce7d0c76262
Philipp <philipp1501@gmx.de>
2020-05-04 17:37:34 +0200

    Networking

--- app/src/main/java/com/example/newtut/MainActivity.java
+++ app/src/main/java/com/example/newtut/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.newtut;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import androidx.fragment.app.FragmentTransaction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +35,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if(findViewById(R.id.fragment_container) != null) {
-            BlankFragment bf = new BlankFragment();
-            //bf.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, bf).commit();
-        }
     }
 
-    public void frag(View view){
-        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-        t.replace(R.id.fragment_container, new BlankFragment2());
-        t.addToBackStack(null);
-        t.commit();
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void network(View view){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        boolean conn = networkCapabilities != null && (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR));
+        Toast.makeText(this, Boolean.toString(conn), Toast.LENGTH_SHORT).show();
+
+        try {
+            URL url = new URL("http://www.google.com");
+            URLConnection urlConnection = url.openConnection();
+            if(urlConnection instanceof HttpURLConnection) {
+                HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.connect();
+                int returncode = httpURLConnection.getResponseCode();
+                if(returncode == HttpURLConnection.HTTP_OK){
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 
     @Override
